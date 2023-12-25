5c4f0cbc275319b6eea1f8a26cce46beabb168f1
Роман Клешнев <time-space@yandex.ru>
2023-12-25 22:04:45 +0300

    Fix views.py, full works, except 5 tests no passed, need to fix profile edit page (passwords dont save)

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 
 from .models import Post, Category, Comment
 from .forms import PostForm, CommentForm
