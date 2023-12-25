2392378130629f1f76527d6cb90ce5a88bd43a81
Роман Клешнев <time-space@yandex.ru>
2023-12-25 11:55:00 +0300

    fix user profile edit page

--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,11 +1,16 @@
 from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.http import Http404
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404
 
 from blog.views import get_queryset
-from .forms import CustomUserEditForm, User
+from .forms import CustomUserEditForm
+
+
+User = get_user_model()
 
 
 class UserCreateView(CreateView):
@@ -33,10 +38,13 @@ def profile(request, username):
 
 
 def edit(request, username):
-    """View функция для страницы редактирования профиля"""
+    """View функция для страницы редактирования профиля."""
     instance = get_object_or_404(User, username=username)
-    form = CustomUserEditForm(request.POST or None, instance=instance)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-    return render(request, 'blog/user.html', context)
+    if instance.username == request.user.username:
+        form = CustomUserEditForm(request.POST or None, instance=instance)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+        return render(request, 'blog/user.html', context)
+    else:
+        raise Http404
